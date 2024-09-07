d69f376af5334a4bda78d22d111f7355d72a0ce2
4th-avenue <quebec706@gmail.com>
2024-09-07 15:32:54 +0900

    City・Country・Employee・State 리소스 폼 필드 수정하고 모델 릴레이션 설정

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Country extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function states(): HasMany
+    {
+        $this->hasMany(Country::class);
+    }
 }
