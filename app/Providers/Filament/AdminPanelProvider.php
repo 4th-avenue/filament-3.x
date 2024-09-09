22536dbf397266c6e96e528d6e24ac49fe18a1ac
4th-avenue <quebec706@gmail.com>
2024-09-09 16:48:27 +0900

    전역 검색 기능 및 내비게이션 뱃지 기능 구현

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,6 +36,11 @@ class AdminPanelProvider extends PanelProvider
                 'warning' => Color::Orange,
             ])
             ->font('Poppins')
+            ->navigationGroups([
+                'Employee Management',
+                'System Management',
+                'User Management'
+            ])
             ->brandName('filament')
             ->brandLogo(asset('images/logo.png'))
             ->favicon(asset('images/favicon.ico'))
