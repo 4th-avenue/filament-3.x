008a18946826528bf7202d269bd93c0672cd1a8d
4th-avenue <quebec706@gmail.com>
2024-09-11 19:53:14 +0900

    관리자 및 사용자 대시보드에 통계 및 차트, 테이블 위젯 추가

--- app/Filament/Widgets/EmployeeAdminChart.php
+++ app/Filament/Widgets/EmployeeAdminChart.php
@@ -2,21 +2,42 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Employee;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 use Filament\Widgets\ChartWidget;
 
 class EmployeeAdminChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Employees Chart';
+
+    protected static ?int $sort = 3;
+
+    protected static string $color = 'warning';
 
     protected function getData(): array
     {
+        $data = Trend::model(Employee::class)
+            ->between(
+                start: now()->startOfMonth(),
+                end: now()->endOfMonth(),
+            )
+            ->perDay()
+            ->count();
+
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Employees',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
         ];
     }
 
     protected function getType(): string
     {
-        return 'bar';
+        return 'line';
     }
 }
--- app/Filament/Widgets/UserAdminChart.php
+++ app/Filament/Widgets/UserAdminChart.php
@@ -2,16 +2,35 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 use Filament\Widgets\ChartWidget;
 
 class UserAdminChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Users Chart';
+
+    protected static ?int $sort = 2;
 
     protected function getData(): array
     {
+        $data = Trend::model(User::class)
+            ->between(
+                start: now()->startOfMonth(),
+                end: now()->endOfMonth(),
+            )
+            ->perDay()
+            ->count();
+
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Users',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
         ];
     }
 
